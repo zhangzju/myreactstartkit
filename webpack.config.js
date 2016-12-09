06c75c8cf98bbfd19dd83fa70e1f2ed3065b89ac
zhangzju <zhangway19921221@163.com>
2016-12-09 18:49:16 +0800

    add test page

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 var webpack = require('webpack');
 var HtmlwebpackPlugin = require('html-webpack-plugin');
 
-var ROOT_PATH = path.resolve(_dirname);
+var ROOT_PATH = path.resolve(__dirname);
 var APP_PATH = path.resolve(ROOT_PATH, 'app');
 var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
 
